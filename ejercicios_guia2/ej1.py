d959439b2ded1376ad18c30db1ab82300b219123
jp-zuniga <jperez070705@gmail.com>
2025-03-28 16:35:07 -0600

    avance de guia 2

---
+++ ejercicios_guia2/ej1.py
@@ -0,0 +1,273 @@
+"""
+Crear un programa que simule la gestión de un inventario en una tienda. Utilizar un menú para
+agregar, eliminar, modificar y consultar productos en el inventario. Cada producto tendrá
+un código, nombre, cantidad y precio.
+"""
+
+from os import system  # para limpiar la pantalla
+from typing import Optional
+
+
+class Producto:
+    """
+    Clase de datos para representar un producto con:
+    - un código,
+    - un nombre,
+    - una cantidad,
+    - un precio
+    """
+
+    def __init__(
+        self,
+        codigo: int = 0,
+        nombre: str = "",
+        cantidad: int = 0,
+        precio: float = 0.0
+    ):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+
+def pedir_codigo(accion: str) -> int:
+    """
+    Pide el código del producto a mostrar/modificar/eliminar.
+    """
+
+    try:
+        codigo = int(input(f"\nIngrese el código del producto que desea {accion}: "))
+        if codigo < 0:
+            raise ValueError
+
+    except (TypeError, ValueError):
+        input("¡El código debe ser un número entero positivo!")
+        return pedir_codigo(accion)  # volver a pedir input
+
+    return codigo
+
+
+def buscar_producto(codigo: int, inventario: list[Producto]) -> Optional[Producto]:
+    """
+    Busca el índice del producto con el código indicado
+    en el inventario de productos registrados.
+    Retorna -1 si el producto no existe.
+    """
+
+    for producto in inventario:
+        if producto.codigo == codigo:
+            return producto
+
+    return None
+
+
+def agregar_producto(inventario: list[Producto]) -> None:
+    """
+    Pide los datos de un producto nuevo y lo agrega al inventario.
+    """
+
+    print("\nAgregar Producto Nuevo:")
+    print("---------------------------------------------")
+
+    print("\nIngrese los datos del producto:")
+    try:
+        codigo = int(input("Código: "))
+        nombre = input("Nombre: ")
+        cantidad = int(input("Cantidad: "))
+        precio = float(input("Precio (en C$): "))
+
+        if codigo < 0 or cantidad < 0 or precio <= 0:
+            raise ValueError
+
+    except (TypeError, ValueError):
+        input(
+            "\n¡El código y la cantidad del producto deben ser números enteros"
+            + " positivos, y el precio debe ser un número real positivo!"
+        )
+
+        system("cls || clear")
+        return agregar_producto(inventario)  # volver a pedir input
+
+    inventario.append(Producto(codigo, nombre, cantidad, precio))
+    input("\n¡Producto agregado exitosamente!")
+    return None
+
+
+def consultar_producto(codigo: int, inventario: list[Producto]) -> None:
+    """
+    Busca un producto específico en el inventario y muestra sus datos.
+    """
+
+    producto = buscar_producto(codigo, inventario)
+    if producto is None:
+        input(f"\n¡No existe un producto con el código '{codigo}'!")
+        return
+
+    print(f"\nProducto #{producto.codigo}:")
+    print("---------------------------------------------")
+    print(f"Nombre: {producto.nombre}")
+    print(f"Cantidad: {producto.cantidad}")
+    print(f"Precio: {producto.precio}")
+    print("---------------------------------------------")
+
+
+def mostrar_productos(inventario: list[Producto]) -> None:
+    """
+    Llama consultar_producto() para todos los productos registrados.
+    """
+
+    print("\nProductos Registrados:")
+    print("-----------------------------------------------------")
+
+    for producto in inventario:
+        consultar_producto(producto.codigo, inventario)
+
+
+def modificar_producto(codigo: int, inventario: list[Producto]) -> None:
+    """
+    Permite al usuario ingresar los datos de un producto de nuevo
+    para modificarlos (excepto el código, ya que se utiliza
+    para identicar los productos).
+    """
+
+    producto_modificado = buscar_producto(codigo, inventario)
+    if producto_modificado is None:
+        input(f"\n¡No existe un producto con el código '{codigo}'!")
+        return None
+
+    # eliminar el producto original para sobreescribirlo
+    inventario.remove(producto_modificado)
+
+    print("\nModificar Producto:")
+    print("---------------------------------------------")
+
+    consultar_producto(codigo, inventario)
+    print()
+
+    # pedir nuevos datos
+    print("\nIngrese los nuevos datos del producto (dejar en blanco para no modificar):")
+    nuevo_nombre = input("Nombre: ")
+    nueva_cantidad = input("Cantidad: ")
+    nuevo_precio = input("Precio: ")
+
+    # los strings vacios se evaluan como False,
+    # entonces si el usuario decide no modificar un atributo,
+    # el valor del objeto original permanecera igual
+
+    if nuevo_nombre:
+        producto_modificado.nombre = nuevo_nombre
+
+    try:
+        if nueva_cantidad:
+            producto_modificado.cantidad = int(nueva_cantidad)
+        if nuevo_precio:
+            producto_modificado.precio = float(nuevo_precio)
+
+        if producto_modificado.cantidad < 0 or producto_modificado.precio <= 0:
+            raise ValueError
+
+    except (TypeError, ValueError):
+        input(
+            "\n¡El código y la cantidad del producto deben ser números enteros"
+            + " positivos, y el precio debe ser un número real positivo!"
+        )
+
+        system("cls || clear")
+        return modificar_producto(codigo, inventario)
+
+    inventario.append(producto_modificado)
+    print(
+        f"\n¡Datos del producto #{producto_modificado.codigo}"
+        + " fueron actualizados exitosamente!"
+    )
+
+    return None
+
+
+def eliminar_producto(codigo: int, inventario: list[Producto]) -> None:
+    """
+    Encuentra el índice del producto a eliminar, y lo
+    quita del inventario de productos.
+    """
+
+    producto = buscar_producto(codigo, inventario)
+    if producto is None:
+        input(f"\n¡No existe un producto con el código '{codigo}'!")
+        return
+
+    print("\nEliminar Producto:")
+    print("---------------------------------------------")
+
+    consultar_producto(codigo, inventario)
+    print()
+
+    confirmacion = input(
+        "\n¿Está seguro que desea eliminar el producto seleccionado? (s/n) "
+    ).lower()
+
+    if confirmacion == "s":
+        inventario.remove(producto)
+        input(f"\n¡Producto #{producto.codigo} eliminado exitosamente!")
+    elif confirmacion == "n":
+        input("\n¡De acuerdo! Regresando al menú principal...")
+    else:
+        input("\n¡Respuesta inválida! Regresando al menú principal...")
+
+
+def menu_principal(inventario: list[Producto]) -> None:
+    """
+    Loop principal del programa que le muestra un menú de opciones al usuario.
+    """
+
+    opcion = ""
+    while opcion != "6":
+        system("cls || clear")
+        print("\nGestión de Inventario")
+        print("-----------------------------------------------------\n")
+        print("1. Agregar producto")
+        print("2. Consultar un producto específico")
+        print("3. Mostrar todos productos registrados")
+        print("4. Modificar producto")
+        print("5. Eliminar producto")
+        print("6. Salir")
+
+        opcion = input("\n-> ")
+
+        match opcion:
+            case "1":
+                system("cls || clear")
+                agregar_producto(inventario)
+            case "2":
+                codigo = pedir_codigo("consultar")
+                system("cls || clear")
+                consultar_producto(codigo, inventario)
+                input("\nPresione 'Enter' para regresar al menú principal...")
+            case "3":
+                system("cls || clear")
+                mostrar_productos(inventario)
+            case "4":
+                codigo = pedir_codigo("modificar")
+                system("cls || clear")
+                modificar_producto(codigo, inventario)
+            case "5":
+                codigo = pedir_codigo("eliminar")
+                system("cls || clear")
+                eliminar_producto(codigo, inventario)
+            case "6":
+                print("\nSaliendo del programa...\n")
+            case _:
+                input("\n¡Opción inválida, intente nuevamente!")
+        inventario.sort(key=lambda p: p.codigo)
+
+
+def main() -> None:
+    """
+    Ejecución del programa.
+    """
+
+    inventario: list[Producto] = []
+    menu_principal(inventario)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ejercicios_guia2/ej2.py
@@ -0,0 +1,114 @@
+"""
+Desarrollar un programa que permita al usuario gestionar una cuenta bancaria.
+El programa deberá utilizar un menú que permita realizar diferentes operaciones
+sobre el saldo de la cuenta. El programa debe permitir al usuario ingresar la
+cantidad para depositar o retirar, actualizar el saldo y mostrar los resultados.
+Si se elige la opción de retiro, debe verificar que el saldo sea suficiente.
+"""
+
+from os import system
+
+
+def consultar_saldo(saldo: float) -> None:
+    """
+    Muestra el saldo de la cuenta del usuario.
+    """
+
+    print("\nEstado de Cuenta:")
+    print("---------------------------------------------")
+    print(f"\nSaldo actual: {saldo:.6f}")
+
+    input("Presione 'Enter' para regresar al menú principal...")
+
+
+def pedir_monto(accion: str) -> float:
+    """
+    Le pide un monto al usuario para realizar
+    una acción con su cuenta bancaria.
+    """
+
+    try:
+        monto = float(input(f"\nIngrese el monto que desea {accion}: "))
+        if monto <= 0:
+            raise ValueError
+    except (TypeError, ValueError):
+        input("\n¡El monto debe ser un número real positivo!")
+        return pedir_monto(accion)
+
+    return monto
+
+
+def depositar(saldo: float) -> float:
+    """
+    Deposita el monto deseado en la cuenta del usuario.
+    """
+
+    monto = pedir_monto("depositar")
+    saldo += monto
+
+    input("\n¡Monto depositado exitosamente!")
+    return saldo
+
+
+def retirar(saldo: float) -> float:
+    """
+    Retira el monto deseado de la cuenta del usuario.
+    """
+
+    monto = pedir_monto("retirar")
+
+    if monto > saldo:
+        input(
+            "\n¡Error! Monto ingresado es mayor que el saldo"
+            + " actual, por favor intente nuevamente..."
+        )
+    else:
+        saldo -= monto
+
+    return saldo
+
+
+def menu_principal(saldo: float) -> None:
+    """
+    Loop principal del programa que le muestra un menú de opciones al usuario.
+    """
+
+    opcion = ""
+    while opcion != "6":
+        system("cls || clear")
+        print("\nGestión de Cuenta Bancaria")
+        print("-----------------------------------------------------\n")
+        print("1. Consultar saldo")
+        print("2. Depositar")
+        print("3. Retirar")
+        print("4. Salir")
+
+        opcion = input("\n-> ")
+
+        match opcion:
+            case "1":
+                system("cls || clear")
+                consultar_saldo(saldo)
+            case "2":
+                system("cls || clear")
+                depositar(saldo)
+            case "3":
+                system("cls || clear")
+                retirar(saldo)
+            case "4":
+                print("\nSaliendo del programa...\n")
+            case _:
+                input("\n¡Opción inválida, intente nuevamente!")
+
+
+def main() -> None:
+    """
+    Ejecución del programa.
+    """
+
+    saldo = 0.0
+    menu_principal(saldo)
+
+
+if __name__ == "__main__":
+    main()
